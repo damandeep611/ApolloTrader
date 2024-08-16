c2e6de5eba25393aac0296497b30704ecddea45a
Damandeep <damandeep611@gmail.com>
2024-08-16 08:55:13 +0530

    test banner

--- src/components/Home/HomeBanner.jsx
+++ src/components/Home/HomeBanner.jsx
@@ -7,7 +7,7 @@ function HomeBanner() {
     <Container>
       <div className="flex flex-col items-center text-center py-5">
         <span className="bg-transparent md:bg-[#E9E5FF] py-[0.25rem] px-[1.18rem] rounded-2xl w-fit mx-auto md:text-base text-[14px] md:max-w-fit max-w-[242px] font-medium text-slate-900">
-          Your Trading Edge Starts Here
+          Your Trading Edge Starts with Apollo
         </span>
         <h1 className="font-semibold md:font-bold text-[1.575rem] md:text-[3rem] leading-[2.25rem] md:leading-[3.43rem] text-slate-900 md:max-w-fit max-w-[279px] mt-[4px] md:mt-[1.5rem] hidden md:block">
           Unleash the Power of Backtesting <br />
